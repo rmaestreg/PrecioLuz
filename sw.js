4128c81d10e818f4aaf5f4d63ac2d9d712dc76e1
rmaestreg <rmaestreg@gmail.com>
2026-07-23 20:32:20 +0200

    Nueva version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "precio-luz-shell-v1.0.23";
+const CACHE_NAME = "precio-luz-shell-v1.0.24";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -82,6 +82,7 @@ self.addEventListener("fetch", event => {
 self.addEventListener("message", event => {
   if (event.data?.type === "SKIP_WAITING") self.skipWaiting();
 });
+
 
 
 
